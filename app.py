6f6d8a20a4b04add02fe6dbb8a92d871932adc2a
Edgar <edgarviajasiempre@gmail.com>
2026-03-08 03:16:01 -0600

    add connection pool

--- app.py
+++ app.py
@@ -143,27 +143,31 @@ import psycopg2
 import psycopg2.extras
 import sqlite3
 
+from psycopg2 import pool
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+connection_pool = None
+
+if DATABASE_URL:
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    connection_pool = psycopg2.pool.SimpleConnectionPool(
+        1,
+        5,
+        DATABASE_URL,
+        sslmode="require"
+    )
+
 def get_db():
 
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    if connection_pool:
+        return connection_pool.getconn()
 
-    if DATABASE_URL:
-
-        if DATABASE_URL.startswith("postgres://"):
-            DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
-        conn = psycopg2.connect(
-            DATABASE_URL,
-            sslmode="require"
-        )
-
-        return conn
-
-    else:
-
-        conn = sqlite3.connect("agencia.db")
-        conn.row_factory = sqlite3.Row
-        return conn
+    conn = sqlite3.connect("agencia.db")
+    conn.row_factory = sqlite3.Row
+    return conn
 # =========================
 # LOGIN
 # =========================
