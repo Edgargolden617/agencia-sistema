8f7350510e39f5c0f99a221235095952e1a6b786
Edgar <edgarviajasiempre@gmail.com>
2026-03-12 00:03:28 -0600

    fix deploy

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 #============= imports =========
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask import Flask, render_template, request, redirect, url_for, session, flash
