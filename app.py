9d8491042f6bfacf3b47217661c7fe82e282870c
Edgar <edgarviajasiempre@gmail.com>
2026-04-24 02:22:36 -0600

    debug real

--- app.py
+++ app.py
@@ -139,18 +139,24 @@ def fecha_corta(valor):
 # =========================
 
 def get_db():
+
     import os
     import psycopg2
+    import psycopg2.extras
 
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    DATABASE_URL = os.environ.get("postgresql://agencia_user:6UNcQOpfjycpFdcn2OGTT6N3bTTyFusy@dpg-d7lhedpo3t8c73f5phog-a.oregon-postgres.render.com/agencia_aixp")
 
-    if not DATABASE_URL:
-        raise Exception("❌ No hay DATABASE_URL en Render")
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-    return psycopg2.connect(
+    conn = psycopg2.connect(
         DATABASE_URL,
-        sslmode="require"
+        sslmode="require",
+        cursor_factory=psycopg2.extras.RealDictCursor
     )
+
+    return conn
+    
 # =========================
 # LOGIN
 # =========================
@@ -482,7 +488,8 @@ def gestionar_reservas():
         JOIN clientes c ON r.cliente_id = c.id
         LEFT JOIN reservacion_hotel h ON r.id = h.reservacion_id
         LEFT JOIN reservacion_avion a ON r.id = a.reservacion_id                     
-
+    --  LEFT JOIN vista_estado_financiero_reserva f
+    --  ON f.reservacion_id = r.id
         {where_sql}
         ORDER BY r.fecha_creacion {orden_sql}
     """, params)
