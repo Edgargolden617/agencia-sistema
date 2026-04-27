30097c93776a4e18095ec433e5865ed096ab640b
Edgar <edgarviajasiempre@gmail.com>
2026-04-27 15:23:23 -0600

    fix gestionar reservas real

--- app.py
+++ app.py
@@ -454,7 +454,7 @@ def gestionar_reservas():
             r.costo_proveedor AS costo_agencia,
             r.estatus,
             r.saldo_a_favor AS saldo_a_favor,
-            COALESCE(r.devolucion_cliente, 0) AS devolucion_ciente,
+            0 AS r.devolucion_cliente,
             COALESCE(0, 0) AS pagado_cliente,
             COALESCE(0, 0) AS pagado_proveedor,
             COALESCE(0, 0) AS saldo_cliente,
